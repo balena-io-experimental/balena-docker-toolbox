96e28092c8e28bb42ae621b3ec565f29a955ea51
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-06-15 15:18:53 +0700

    1st commit

---
+++ automation/jenkins-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+REPO='resin/docker-toolbox'
+
+for arch in $ARCHS; do
+	cp -f Dockerfile.$arch Dockerfile
+	docker build -t $REPO:$arch .
+done
+
+docker push $REPO
